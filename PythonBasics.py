917f2734803e0c822f78459e16ce0e211c41b24e
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-14 23:28:50 +0530

    re-freshing up the basics

---
+++ PythonBasics.py
@@ -0,0 +1,15 @@
+name = input("what is youor name ? ")
+print("heyyyyy " + name + " how is life going on ?")
+print(type(7*7))
+
+print(bin(5))
+print(int("0b101",2)) #this is used to convert the binary form to the Int form and the 2 in the parameter pf the int is the form of the type which we have written i.e, we have written in binary form and hence that is the reason why we have put 2 not 10 (because 10 is used for decimals not for the binary)
+
+a = 10 #no need to declare the variable for storing the TYPE i.e, int  a = 10 is wrong syntax
+b = 21
+print(a,b)
+
+c,d,e = 4,5,6 #we can do assign like this
+print(a,b,c,d,e)
+
+
