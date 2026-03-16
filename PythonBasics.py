58b133d8aa8a15285f54336a72da3ede282e6b77
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-16 22:21:11 +0530

    learnt basic type conversions

--- PythonBasics.py
+++ PythonBasics.py
@@ -40,3 +40,11 @@ print("hello " + "shubham ") #but we can only type the stings for concatenations
 print(type(str(1408))) #-------> it is converted to string form
 #now converting it to the integer form
 print(type(int(str(1408)))) #-----> this will be in  the integer form as the string form is converted to the integer form
+'''the other form of writing this and which is more easy to understand is :-'''
+a=100
+b=str(a) #------> makes 100 integer to string 
+e= int(b) #------> makes the converted 100 in string to integer again 
+c=type(b)
+print(c)
+d=type(e)
+print(d)
