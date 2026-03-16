0dfcb6c0efd2cdf2ee725dfeea029b3ea9ed8924
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-16 21:28:40 +0530

    Brushed the basics of the python agian

--- PythonBasics.py
+++ PythonBasics.py
@@ -2,6 +2,8 @@ name = input("what is youor name ? ")
 print("heyyyyy " + name + " how is life going on ?")
 print(type(7*7))
 
+
+
 print(bin(5))
 print(int("0b101",2)) #this is used to convert the binary form to the Int form and the 2 in the parameter pf the int is the form of the type which we have written i.e, we have written in binary form and hence that is the reason why we have put 2 not 10 (because 10 is used for decimals not for the binary)
 
@@ -13,3 +15,20 @@ c,d,e = 4,5,6 #we can do assign like this
 print(a,b,c,d,e)
 
 
+#---------------------------------------- #NEXT DAY
+
+#all about the string data type
+print(type("Heyy shubham how you doing today ")) #note we can also type the sting in ' ' single strings
+long_string = ''' 
+wwwww 
+*   *
+-----
+''' #now i can write the long string in multiple line unlike the single and the double strings
+print(long_string)
+
+
+#we can add the two different strings just like how we do for the Integers addition
+firstName = 'shubham'
+lastName =  'ojha'
+full_name = firstName + ' ' + lastName 
+print(full_name)
