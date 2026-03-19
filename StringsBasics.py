44f8fc2bd01b5628c244cc24af9f47d06e4501af
Shubham Ojha <shubham4clgg@gmail.com>
2026-03-19 14:00:31 +0530

    learn escaping sequence basics

--- StringsBasics.py
+++ StringsBasics.py
@@ -1,31 +1,13 @@
-#all about the string data type
-print(type("Heyy shubham how you doing today ")) #note we can also type the sting in ' ' single strings
-long_string = ''' 
-wwwww 
-*   *
------
-''' #now i can write the long string in multiple line unlike the single and the double strings
-print(long_string)
+#Escape Sequence
 
-
-#we can add the two different strings just like how we do for the Integers addition
-firstName = 'shubham'
-lastName =  'ojha'
-full_name = firstName + ' ' + lastName 
-print(full_name)
-#string concatenations
-print("hello " + "shubham ") #but we can only type the stings for concatenations i.e, this is wrong print("hello " + "14 ")
-
-#Type casting
-''' we can convert the integer type data to the string type by just doing the following operations'''
-print(type(str(1408))) #-------> it is converted to string form
-#now converting it to the integer form
-print(type(int(str(1408)))) #-----> this will be in  the integer form as the string form is converted to the integer form
-'''the other form of writing this and which is more easy to understand is :-'''
-a=100
-b=str(a) #------> makes 100 integer to string 
-e= int(b) #------> makes the converted 100 in string to integer again 
-c=type(b)
-print(c)
-d=type(e)
-print(d)
+print("Shubh:\they") #this gives a tab space after : . OUTPUT: Shubh:  hey
+print("how is \nshivamogga ?") #this tells that start the word shivamogga from next line. OUTPUT: how is
+                                                                                                    #shivamogga ?
+print("what work we have to do in \"ADA\" ") # after this \ python counts everything as a string but there shouldn't be any space between "\ "ada \" " . OUTPUT: what work we have to do in "ADA".
+print("C:\\BACKEND\\StringsBasics") # this counts the inner \\ i.e, the second back slash as a string. OUTPUT: C:\BACKEND\StringsBasics 
+print(r"hello\nworld") # if you want escape sequence to not be executed then preceeded with r .
+print ("it's fine")
+print("He called me 'yesterday'")
+#or the second way 
+print('He called me "yesterday" ')
+print('what did mam teach yesterday in "ADA" ') 
